940ed8377326bb535bbe70114b767724d195dcaa
Ahmed Abdirizaak <mernstack.js@gmail.com>
2022-03-20 00:50:50 +0100

    new V

--- src/App.js
+++ src/App.js
@@ -10,9 +10,42 @@ function App() {
     };
     fetchData();
   }, []);
+
+  const [book, setBook] = useState({
+    username: "",
+    bookname: "",
+  });
+
+  const onChnage = ({ target }) => {
+    setBook({ ...book, [target.name]: target.value });
+  };
+  const onSubmitHandel = (e) => {
+    e.preventDefault();
+    const fetchDataPost = async () => {
+      await axios
+        .post("https://whispering-shore-13501.herokuapp.com/book", book)
+        .then((res) => console.log(res.data));
+    };
+    fetchDataPost();
+  };
   return (
     <div>
       App
+      <form onSubmit={onSubmitHandel}>
+        <input
+          placeholder="username"
+          value={book.username}
+          name="username"
+          onChange={onChnage}
+        />
+        <input
+          placeholder="username"
+          value={book.bookname}
+          name="bookname"
+          onChange={onChnage}
+        />
+        <button>Add</button>
+      </form>
       {data.map((item) => (
         <div>
           <h1>{item.bookname}</h1>
